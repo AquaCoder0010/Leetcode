b0d4bab62db86894053e7737895a7c4a0b52090b
Beemnet Asegid <93863375+AquaCoder0010@users.noreply.github.com>
2024-03-31 12:39:47 +0300

    Time: 18 ms (95.50%) | Memory: 22.1 MB (94.33%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -2,54 +2,42 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         std::vector<std::pair<std::string, int>> groupedPair;
-    std::vector<std::vector<std::string>> groupedStr;
+        std::vector<std::vector<std::string>> groupedStr;
 
-    for(int i = 0; i < strs.size(); i++)
-    {
-        std::string sortedString = strs[i];
-        std::sort(sortedString.begin(), sortedString.end());
-        groupedPair.emplace_back();
-        groupedPair.back().first = sortedString;
-        groupedPair.back().second = i;    
-    }
-    std::sort(groupedPair.begin(), groupedPair.end(), [&](std::pair<std::string, int>& pair1, std::pair<std::string, int>& pair2)
-    {
-        return pair1.first > pair2.first;
-    });
-
-    for(auto& str : strs)
-    {
-        std::cout << str << ",";
-    }
-    std::cout << '\n';
-
-
-    for(auto& pair : groupedPair)
-    {
-        std::cout << pair.first << "," << strs[pair.second] << ",index:" << pair.second << ", ";  
-    }
-    std::cout << std::endl;
-
-    std::string currentString = groupedPair[0].first;
-    std::vector<std::string> currentGroup;
-
-    for(int i = 0; i < groupedPair.size(); i++)
-    {
-        if(currentString == groupedPair[i].first)
+        for(int i = 0; i < strs.size(); i++)
         {
-            currentGroup.emplace_back(strs[groupedPair[i].second]);
+            std::string sortedString = strs[i];
+            std::sort(sortedString.begin(), sortedString.end());
+            groupedPair.emplace_back();
+            groupedPair.back().first = sortedString;
+            groupedPair.back().second = i;    
         }
-        if(currentString != groupedPair[i].first)
+        std::sort(groupedPair.begin(), groupedPair.end(), [&](std::pair<std::string, int>& pair1, std::pair<std::string, int>& pair2)
         {
-            groupedStr.emplace_back(currentGroup);
-            currentGroup.clear();
+            return pair1.first > pair2.first;
+        });
 
-            currentString = groupedPair[i].first;
-            currentGroup.emplace_back(strs[groupedPair[i].second]);
-        }
-        if(i == groupedPair.size() - 1)
-            groupedStr.emplace_back(currentGroup);
-        }
-        return groupedStr;  
+
+        std::string currentString = groupedPair[0].first;
+        std::vector<std::string> currentGroup;
+
+        for(int i = 0; i < groupedPair.size(); i++)
+        {
+            if(currentString == groupedPair[i].first)
+            {
+                currentGroup.emplace_back(strs[groupedPair[i].second]);
+            }
+            if(currentString != groupedPair[i].first)
+            {
+                groupedStr.emplace_back(currentGroup);
+                currentGroup.clear();
+
+                currentString = groupedPair[i].first;
+                currentGroup.emplace_back(strs[groupedPair[i].second]);
+            }
+            if(i == groupedPair.size() - 1)
+                groupedStr.emplace_back(currentGroup);
+            }
+            return groupedStr;  
     }
 };
